2c28e16ce8f47515a2578295903496abd04b1c5e
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-04 02:25:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,10 @@ class Anagram
 
   def match(array)
     array_matches = []
+    word_to_check_split = word_to_check.split("")
     array.each do |word|
       word_array = word.split("")
-       if word_array == @word_to_check
+      if word_array.sort == word_to_check_split.sort
         array_matches << word
       end
     end
