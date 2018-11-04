82db8e93ae78c2424dbbb5e67d425f6dcf0a2425
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-04 01:44:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,10 @@ class Anagram
   def initialize(word)
     @word_to_check = word
   end
-
+#take word on initialization and then compare it to a 
+#array of words in match
   def match(array)
+  #in match, take the word from initialize and run throug
     array.split("")
 
     a.select { |v| v =~ /[aeiou]/ }  #=> ["a", "e"]
