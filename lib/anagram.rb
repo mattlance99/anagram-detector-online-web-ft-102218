244a497c23c268bed5a54234fd1237fe7f54632b
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-04 01:40:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,8 @@ class Anagram
   end
 
   def match(array)
+    if @word_to_check == array.split("")
+    end
     a.select { |v| v =~ /[aeiou]/ }  #=> ["a", "e"]
     @word_to_check = array
     array_matches
