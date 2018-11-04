e8e3f6dfe13de99068a5b8ff3f267e9cbfeb764d
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-04 01:58:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,9 @@ class Anagram
 #take word on initialization and then compare it to a 
 #array of words in match
   def match(array)
-    array.each do |word|
-      word_array = word.split("")
-    end
+    #array.each do |word|
+      #word_array = word.split("")
+    #end
     
   end  
   #in match, run through the array and compare it to the word from initialize. return an array that has an anagram.
