4011dff92cd90ce03d105b8438613b8d6aed8430
Matthew Cohen <matt.cohen182@gmail.com>
2018-11-04 01:10:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :array_matches
+  @@email_array
+
+  def initialize(word)
+    @emails = word
+  end
+
+  def match(array)
+    @array_matches = array
+    array_matches
+  end
+end
